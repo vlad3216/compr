2b625e311025658a1ae72f34900173007387b1cb
vlad3216 <103311342+vlad3216@users.noreply.github.com>
2022-04-15 13:30:04 +0300

    Update calculator2.py

--- calculator2.py
+++ calculator2.py
@@ -3,7 +3,7 @@ operation = (input("Выберите операцию(+, - , * ,/): "))
 number_1 = int(input("Введи первое целое число: "))
 number_2 = int(input("Введи второе целое число: "))
 if operation == "+":
-   print(number_1 + number_2)
+    print(number_1 + number_2)
 elif operation == "-":
     print(number_1 - number_2)
 elif operation ==  "*":
