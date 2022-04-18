90dfec20c8163a535fa5ec7caab65fcb18307b48
vlad3216 <103311342+vlad3216@users.noreply.github.com>
2022-04-18 18:07:40 +0300

    Add files via upload

---
+++ find_cucumber.py
@@ -0,0 +1,59 @@
+print('давай попробуем найти огурец 🥒')
+text = (input('Введи фразу со словом "cucumber": '))
+x = 'cucumber'
+if x in text:
+    print(text[text.find(x) + 0 :])
+elif x not in text:
+    print('Next time buddy')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
